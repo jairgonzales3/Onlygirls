528b3166cd13e73c8ce40131e98c7018453c7f44
Jair Gonzales <108778872+jairgonzales3@users.noreply.github.com>
2022-08-16 17:23:46 -0500

    Update info-estado.js

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -8,7 +8,7 @@ _muptime = await new Promise(resolve => { process.once('message', resolve)
 setTimeout(resolve, 1000) }) * 1000}
 let uptime = clockString(_uptime)
 let estado =`
-╭─[ *@ffxryu.18* ]
+╭─[ *𝐎𝐧𝐥𝐲 𝐆𝐢𝐫𝐥𝐬* ]
 │ *➤ 𝙷𝙾𝙻𝙰 ${name}*
 │
 │ *ミ🤖 𝙴𝚂𝚃𝙰𝙳𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃 🤖彡*
